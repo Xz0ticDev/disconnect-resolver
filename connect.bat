8883180f625a1f5f63fd9ed83dfa7005380a1103
Alex Troiani <atroiani6@gmail.com>
2016-10-08 05:30:29 -0700

    Update connect.bat

--- connect.bat
+++ connect.bat
@@ -1,6 +1,5 @@
 @echo off
 
 for /f "tokens=*" %%f in (ssid.txt) do set "ssid=%%f"
-for /f "tokens=*" %%f in (interface.txt) do set "interface=%%f"
 
-netsh wlan connect name="%ssid%" ssid="%ssid%" interface=%interface%
+netsh wlan connect name="%ssid%" ssid="%ssid%"
