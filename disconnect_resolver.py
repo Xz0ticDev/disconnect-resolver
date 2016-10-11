8675881bc0b4695f3c7d1203eb4c456f81f24c0b
Alex Troiani <atroiani6@gmail.com>
2016-10-10 23:24:03 -0700

    Error and data logging
    now logs errors in red, displays average ping

--- disconnect_resolver.py
+++ disconnect_resolver.py
@@ -2,9 +2,12 @@ import os
 import time
 import winsound
 from subprocess import Popen
+from termcolor import colored, cprint
 
-#server to test ping
+#declare variables
 pingServer = "8.8.8.8"
+totalPings = 0
+numPings = 0
 
 #call batch file to disconnect from internet
 def disconnect():
@@ -20,29 +23,47 @@ def connect():
 def ping(host):
 
     #return ping true or false
-    print (str)(os.system("ping -n 1 " + host) == 0)
-    return os.system("ping -n 1 " + host) == 0
+    return os.system('ping -n 1 ' + host + " > nul") == 0
 
 #disconnect and reconnect to internet if  connection error was detected
 def resolve():
-    if(ping(pingServer) == False or getRawPing() > 100):
-        disconnect()
-        time.sleep(2)
-        connect()
-        time.sleep(2)
 
+    #checks for connection interruption
+    if(ping(pingServer) == False):
+        cprint("\n\nERROR: Connection interrupted.", 'red', attrs = ['bold'])
+        disconnect()
+        time.sleep(1)
+        connect()
+        time.sleep(1)
+
+    #checks for high ping
+    elif(getRawPing() > 100):
+        cprint("\n\nERROR: High latency.", 'red', attrs = ['bold'])
+        disconnect()
+        time.sleep(1)
+        connect()
+        time.sleep(1)
+
+#returns ping as integer
 def getRawPing():
+    global totalPings
+    global numPings
     ping = os.popen('ping ' + pingServer + ' -n 1')
     result = ping.readlines()
     msLine = result[-1].strip()
     ms = msLine[len(msLine) - 4:len(msLine) - 2]
-    print ms
+    totalPings += int(ms)
+    numPings += 1
+    print '\r{0}'.format("Average ping: " + str(totalPings / numPings) + "ms"),
     return int(ms)
 
-#loop program with 2 second interval
-while(True):
-    resolve()
-    time.sleep(2)
+#main function
+if __name__ == "__main__":
+
+    #loop program with 2 second interval
+    while(True):
+        resolve()
+        time.sleep(2)
 
 #exit program
 exit()
